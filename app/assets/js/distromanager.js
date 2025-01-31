e27024f41216f2bdbc1ed842e55222405cb7c093
askull1 <nuclshine@gmail.com>
2025-01-31 21:28:21 +0900

    yy

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://askull1.github.io/mcserver/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
